43fad13ee9fe2e2326c741c66df9dcd6de38fb62
asterik <vikingsfortune@gmail.com>
2025-12-15 21:12:48 +1030

    updated inngest files

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,15 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { deleteCouponOnExpiry, syncUserCreation, syncUSerDeletion, syncUserUpdation } from "@/inngest/functions";
+import { deleteCouponOnExpiry, syncUserCreation, syncUSerDeletion, syncUserUpdation } from "@/inngest/function";
+
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    syncUserCreation, syncUserUpdation, syncUSerDeletion, deleteCouponOnExpiry
+    syncUserCreation,
+    syncUserUpdation,
+    syncUSerDeletion,
+    deleteCouponOnExpiry,
   ],
 });
